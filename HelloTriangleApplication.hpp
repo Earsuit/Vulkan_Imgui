0e9f2c2225465d07bc8e92acbca4ad1a84a6b029
earsuit <renyudicn@outlook.com>
2020-08-24 15:14:06 +0800

    staging buffer

--- HelloTriangleApplication.hpp
+++ HelloTriangleApplication.hpp
@@ -159,6 +159,12 @@ private:
     void cleanupSwapChain();
     void createVertexBuffer();
     uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void createBuffer(VkDeviceSize size,
+                      VkBufferUsageFlags usage,
+                      VkMemoryPropertyFlags properties,
+                      VkBuffer& buffer,
+                      VkDeviceMemory& bufferMemory);
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
 };
 
 #endif
