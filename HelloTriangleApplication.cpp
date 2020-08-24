fd85166bd442b5b8a4bbfa82a5186868dc30c209
earsuit <renyudicn@outlook.com>
2020-08-24 13:39:45 +0800

    fix bug

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -102,8 +102,6 @@ void HelloTriangleApplication::cleanup()
     vkDestroyBuffer(device, vertexBuffer, nullptr);
     vkFreeMemory(device, vertexBufferMemory, nullptr);
 
-    vkDestroyBuffer(device, vertexBuffer, nullptr);
-
     for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
         vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
         vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
